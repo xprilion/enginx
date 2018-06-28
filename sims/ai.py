70506eeaf03ad109789646bc632d81a42fddfeb7
Anubhav Singh <anubhav.ssd@gmail.com>
2018-06-28 22:25:17 +0530

    ai coming soon

--- sims/ai.py
+++ sims/ai.py
@@ -1,4 +1,23 @@
 # Welcome, to the world's coolest learner algorithm
 
+import json
 
+lastfile = "data/algo/last.json"
+tlsfile = "data/algo/tls.json"
+
+rundata = []
+
+with open(lastfile, "r") as l:
+	for line in l:
+		rundata.append(json.loads(line))
+
+with open(tlsfile, "r") as t:
+	tlsdata = json.load(t)
+
+for r in rundata:
+	print(r)
+
+print("\n\n\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n")
+
+print(tlsdata)
 
